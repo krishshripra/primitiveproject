6cb78243e5bd1463ed95edd11e78a5963a90abd7
shrik <shrikrishna.dound@gmail.com>
2023-07-23 08:36:05 +0530

    this new code

---
+++ eclipse-workspace/github/src/github/demo1.java
@@ -0,0 +1,18 @@
+package github;
+
+public class demo1 {
+
+	public static void main(String[] args) {
+		int a,b,c,d;
+		d=100;
+		c=d;
+		a=c+d;
+		b=a+c+d;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+
+	}
+
+}
