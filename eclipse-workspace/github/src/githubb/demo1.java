810f3786e1c7317f5037773c9147142ce7d8d926
shrik <shrikrishna.dound@gmail.com>
2023-07-25 09:21:49 +0530

    this is new code

---
+++ eclipse-workspace/github/src/githubb/demo1.java
@@ -0,0 +1,15 @@
+package githubb;
+
+public class demo1 {
+
+	public static void main(String[] args) {
+		byte b=10;
+		int i=20;
+		int c=b+i;
+		System.out.println(i);
+		System.out.println(b);
+		System.out.println(c);
+
+	}
+
+}
