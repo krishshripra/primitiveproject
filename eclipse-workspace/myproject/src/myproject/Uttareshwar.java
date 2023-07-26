b462936ea12d207dde76fcb80c0f34d19c514507
DELL <uvwpaps87@gmail.com>
2023-07-26 12:43:07 +0530

    this is my commit

---
+++ eclipse-workspace/myproject/src/myproject/Uttareshwar.java
@@ -0,0 +1,22 @@
+package myproject;
+
+public class Uttareshwar {
+
+	public static void main(String[] args) {
+	
+		int a=100, b=50;
+		System.out.println(a);
+		System.out.println(b);
+		int c=a+b;
+		System.out.println("Addition= "+c);
+		int d=a-b;
+		System.out.println("Substraction= "+d);
+		int e=a*b;
+		System.out.println("Multiplication= "+e);
+		int f=a/b;
+		System.out.println("Division= "+f);
+
+
+	}
+
+}
