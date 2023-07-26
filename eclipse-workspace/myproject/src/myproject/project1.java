d155c7eac916cb68fce4c5135b01be8d885855c1
shrik <shrikrishna.dound@gmail.com>
2023-07-26 09:55:06 +0530

    this is my new project

---
+++ eclipse-workspace/myproject/src/myproject/project1.java
@@ -0,0 +1,17 @@
+package myproject;
+
+public class project1 {
+
+	public static void main(String[] args) {
+        byte a=10;
+        short b=20;
+        int c=a+b;
+        long d=c;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+	}
+
+}
