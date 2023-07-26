2a45614978211d9c7e427e3eabf3acfac3e357a0
shrik <shrikrishna.dound@gmail.com>
2023-07-26 11:25:22 +0530

    project2

---
+++ eclipse-workspace/myproject/src/myproject/project2.java
@@ -0,0 +1,15 @@
+package myproject;
+
+public class project2 {
+
+	public static void main(String[] args) {
+		long l=10102020;
+		int i=120120;
+		double d=l+i;
+		System.out.println(l);
+		System.out.println(i);
+		System.out.println(d);
+
+	}
+
+}
